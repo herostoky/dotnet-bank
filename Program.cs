fb7ec581bac172c58cd3c72e3e600facebcbb723
Toky <Toky.SOLOFONIRINA@pulse.mg>
2023-02-07 18:37:52 +0300

    Up project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bank
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            //double amount = 10_000_000;
+            //int durationInMonth = 12;
+            //int periodicity = 1;
+            //double valueWitoutRate = services.BankService.getAmountPerPeriod(amount, durationInMonth, periodicity);
+            //double value = services.BankService.getAmountPerPeriodWithRate(amount, durationInMonth, periodicity);
+        }
+    }
+}
---
+++ services/BankService.cs
@@ -0,0 +1,34 @@
+﻿namespace bank.services
+{
+    class BankService
+    {
+        static double getRate(double totalAmount, int durationInMonth)
+        {
+            double rate = 0.0;
+            if(totalAmount <= 5_000_000)
+            {
+                if (durationInMonth <= 12) rate = 2.0;
+                else if (durationInMonth <= 24) rate = 3.0;
+                else rate = (durationInMonth / 5);
+            }
+            else
+            {
+                if (durationInMonth <= 12) rate = 1.5;
+                else if (durationInMonth <= 24) rate = 2.5;
+                else rate = (durationInMonth / 6.5);
+            }
+            return rate;
+        }
+        public static double getAmountPerPeriod(double totalAmount, int durationInMonth, int periodicity)
+        {
+            double ret = ((totalAmount * periodicity) / durationInMonth);
+            return ret;
+        }
+        public static double getAmountPerPeriodWithRate(double totalAmount, int durationInMonth, int periodicity)
+        {
+            double rate = getRate(totalAmount, durationInMonth);
+            double amountPerPeriod = getAmountPerPeriod(totalAmount, durationInMonth, periodicity);
+            return (amountPerPeriod * (1 + (rate / 100)));
+        }
+    }
+}
